e7edf1e2054250e8c74ccc3e51e3251ee4982624
j0hn3 <19431159+j0hn3@users.noreply.github.com>
2020-08-02 19:15:11 -0400

    created read and write as json modules

---
+++ ip_address_scheme/ip_address_scheme.py
@@ -0,0 +1,8 @@
+import ipaddress
+import sys 
+import read_write_as_json
+
+if __name__ == "__main__":
+    address_scheme = read_write_as_json.open_address_scheme(input("Enter the file name of the address scheme to open or leave blank for ip_address_scheme.json: ") or "ip_address_scheme.json")
+    print(address_scheme)
+    
