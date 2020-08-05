b91d0fd6d3b80934ba983228d1495d3e1acd99ab
j0hn3 <19431159+j0hn3@users.noreply.github.com>
2020-08-05 17:59:08 -0400

    updated folder structure

--- python-network-mgmt/ip_address_scheme.py
+++ python-network-mgmt/ip_address_scheme.py
@@ -4,5 +4,6 @@ import read_write_as_json
 
 if __name__ == "__main__":
     address_scheme = read_write_as_json.open_address_scheme(input("Enter the file name of the address scheme to open or leave blank for ip_address_scheme.json: ") or "ip_address_scheme.json")
+    print(type(address_scheme))
     print(address_scheme)
     
