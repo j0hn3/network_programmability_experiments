fdc511a90894ba51e95613e3817a5eaa6e547d75
j0hn3 <19431159+j0hn3@users.noreply.github.com>
2020-08-07 19:05:51 -0400

    created reserve_dc_subnet.py with steps for script to take

---
+++ python-network-mgmt/reserve_dc_subnet.py
@@ -0,0 +1,4 @@
+1. open file creatd by gen_dc_addr_plan
+2. reserve a subnet at each DC by changing the value from available to reserved snf and the system name
+3. save the updated address plan with a timestamp indicating when it was updated 
+
