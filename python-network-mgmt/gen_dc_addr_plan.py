ac8f585db78a376217ff2ebd10a1436c25425511
j0hn3 <19431159+j0hn3@users.noreply.github.com>
2020-08-08 20:06:31 -0400

    added VLAN  capabilities
    added generation of VLAN range to scheme and reservation capabilities

--- python-network-mgmt/gen_dc_addr_plan.py
+++ python-network-mgmt/gen_dc_addr_plan.py
@@ -7,7 +7,7 @@ all_dcs_supernet = "10.100.0.0/16"
 #supernet to be divided among all DCs
 per_dc_supernet = 18 
 #CIDR for per DC supernet
-prefix_for_dc_subnets = 26 
+prefix_for_dc_subnets = 27 
 #CIDR for host subnet in each DC
 datacenters = ['east', 'west', 'north', 'south', ] 
 #names of datacenters
@@ -18,28 +18,41 @@ backup_output_file_name = 'dc_address_plan_original.json'
 
 ###script###
 
+VLANS = list(range(1005,4095))
+#generate a list of extended range VLANS
+
 supernet = ipaddress.IPv4Network(all_dcs_supernet)
 #typecast supernet into ipaddress.network object
 
 dc_supernets = [str(s) for s in supernet.subnets(new_prefix = per_dc_supernet)]
 #create a list of supernets for each dc from all_dcs_supernet
 
-dc_ip_scheme = {}
+dc_ip_scheme = {'datacenters': {} }
 #create dict to store address plan
 
 for item in range(len(datacenters)):
-    dc_ip_scheme[datacenters[item]] = {'supernet' : dc_supernets[item]}
+    dc_ip_scheme['datacenters'][datacenters[item]] = {'supernet' : dc_supernets[item]} 
+    dc_ip_scheme['datacenters'][datacenters[item]]['IPv4_Addresses'] = {}
+
+#for item in range(len(datacenters)):
+#    dc_ip_scheme[datacenters[item]]= {'supernet' : dc_supernets[item]}
 #add data to the dc_ip_scheme in the following format dc_ip_scheme{ datacenter { supernet :  dc_supernet }}
 
-for dc in dc_ip_scheme.keys():
-    networks =  ipaddress.IPv4Network(dc_ip_scheme[dc]['supernet']).subnets(new_prefix = prefix_for_dc_subnets)
+for dc in dc_ip_scheme['datacenters'].keys():
+    networks =  ipaddress.IPv4Network(dc_ip_scheme['datacenters'][dc]['supernet']).subnets(new_prefix = prefix_for_dc_subnets)
     for ip in networks:
-       dc_ip_scheme[dc][str(ip)] = 'available'
+       dc_ip_scheme['datacenters'][dc]['IPv4_Addresses'][str(ip)] = 'available'
 #add a list of all the /24s in the supernet assigned to each dc 
 #format is dc_ip_scheme[dc][subnet] =  'available'
 #available assigned as value as it will be changed later when allocated 
 
+for dc in dc_ip_scheme['datacenters'].keys():
+    dc_ip_scheme['datacenters'][dc]['VLANS'] = {}
+    for VLAN in VLANS:
+        dc_ip_scheme['datacenters'][dc]['VLANS'][VLAN] = 'available'
+
 dc_ip_scheme['update_log'] = {}
+#create a new dictionary entry to track updates 
 
 with open(output_file_name, 'w') as file:
     file.write(json.dumps(dc_ip_scheme, indent=4))
--- python-network-mgmt/reserve_dc_subnet.py
+++ python-network-mgmt/reserve_dc_subnet.py
@@ -13,18 +13,32 @@ def open_address_scheme(file_name = 'dc_address_plan.json'):
     return dc_address_plan
 
 def reserve_subnet( dc_address_plan, req ):
-    allocation = {}
+    ip_allocation = {}
     #create a dict to store a copy subnets allocated in the format {dc:network} 
-    for dc in dc_address_plan.keys():
-        for subnet, status in dc_address_plan[dc].items():
+    for dc in dc_address_plan['datacenters'].keys():
+        for subnet, status in dc_address_plan['datacenters'][dc]['IPv4_Addresses'].items():
         #for each dc loop through all the enties until you find the first subnet (key) which has a value of 'available'
             if status != 'available':
                 continue
             elif status == 'available':
-                allocation[dc] = subnet
-                dc_address_plan[dc][subnet] = ['reserved', req, ]
+                ip_allocation[dc] = subnet
+                dc_address_plan['datacenters'][dc]['IPv4_Addresses'][subnet] = ['reserved', req, ]
                 break
-    return dc_address_plan, allocation
+    return dc_address_plan, ip_allocation
+
+def reserve_vlan( dc_address_plan, req ):
+    vlan_allocation = {}
+    for dc in dc_address_plan['datacenters'].keys():
+        for vlan, status in dc_address_plan['datacenters'][dc]['VLANS'].items():
+        #for each dc loop through all the enties until you find the first subnet (key) which has a value of 'available'
+            if status != 'available':
+                continue
+            elif status == 'available':
+                vlan_allocation[dc] = vlan
+                dc_address_plan['datacenters'][dc]['VLANS'][vlan] = ['reserved', req, ]
+                break
+    return dc_address_plan, vlan_allocation
+
 
 def save_address_scheme(dc_address_plan, req, file_name = 'dc_address_plan.json'):
     now = datetime.datetime.now()
@@ -35,14 +49,26 @@ def save_address_scheme(dc_address_plan, req, file_name = 'dc_address_plan.json'
 
 
 
-def print_allocation(req, allocation):
+def print_allocation(req, ip_allocation, vlan_allocation):
     print(f"""
     ########################################################
     For {req} you have reserved the following address space: 
-    East DC: {allocation['east']}
-    West DC: {allocation['west']} 
-    North DC: {allocation['north']} 
-    South DC: {allocation['south']} 
+    
+    East DC 
+    Subnet: {ip_allocation['east']} 
+    VLAN: {vlan_allocation['east']}
+
+    West DC 
+    Subnet: {ip_allocation['west']}
+    VLAN: {vlan_allocation['west']}
+
+    North DC 
+    Subnet: {ip_allocation['north']}
+    VLAN: {vlan_allocation['north']} 
+
+    South DC
+    Subnet: {ip_allocation['south']}
+    VLAN: {vlan_allocation['south']}
     ########################################################
     """)
 
@@ -52,9 +78,11 @@ if __name__ == "__main__":
     #get a requirement number for the allocation
     dc_address_plan = open_address_scheme()
     #open the file containaining the address plan for the dc
-    dc_address_plan, allocation = reserve_subnet( dc_address_plan, req )
+    dc_address_plan, ip_allocation = reserve_subnet( dc_address_plan, req )
     #reserve the first available subnet at each DC
-    save_address_scheme(dc_address_plan, req)
+    dc_address_plan, vlan_allocation = reserve_vlan( dc_address_plan, req )
+    print(vlan_allocation)
+    dc_address_plan, save_address_scheme(dc_address_plan, req)
     #save the updated address plan for the dc
-    print_allocation(req, allocation)
+    print_allocation(req, ip_allocation, vlan_allocation)
     #print the reserved subnets
