d2167485480c482600d3feed14f7aefb39022875
j0hn3 <19431159+j0hn3@users.noreply.github.com>
2020-08-15 13:10:44 -0400

    building out framework for tunnel_builder

---
+++ python-tunnel-builder/gen_tunnel_addr_plan.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python3.7
+
+import ipaddress
+import tunnel_addr_plan_input_vars
+import file_ops
+
+
+if __name__ == "__main__":
+    
---
+++ python-tunnel-builder/tunnel_addr_plan_input_vars.py
@@ -0,0 +1,3 @@
+#this file contails the variables used to generate an address plan for the tunnel_builder.py 
+
+tunnel_supernet = '192.168.254.0/24'
