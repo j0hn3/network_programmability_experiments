8e4188222733ed5fb2028d04ef72042a1e81d4cf
j0hn3 <19431159+j0hn3@users.noreply.github.com>
2020-08-19 20:38:22 -0400

    added save new gold config script

--- virtual-box-lab/lab_device_ops.py
+++ virtual-box-lab/lab_device_ops.py
@@ -6,13 +6,13 @@ import lab_file_ops
 def open_ssh_con(inventory):
 #open an ssh connection to each device marked as reachable in the inventory and ...
 #return and updated inventory
-
     for device in inventory.keys():
         device_info = {
             "device_type": inventory[device]['netmiko-type'],
             "host": inventory[device]['mgmt-ip'],
             "username": inventory[device]['lab-user'],
             "password": inventory[device]['lab-pass'],
+            "secret": inventory[device]['secret'],
         }
         try:
             net_connect = ConnectHandler(**device_info)
@@ -26,10 +26,32 @@ def open_ssh_con(inventory):
         except:
             pass
         #try to enter privilidged mode if the device type supports it
-
         inventory[device]['ssh_con'] = net_connect
-
     return inventory
+
+def open_single_ssh_con(inventory, device):
+    device_info = {
+    "device_type": inventory[device]['netmiko-type'],
+    "host": inventory[device]['mgmt-ip'],
+    "username": inventory[device]['lab-user'],
+    "password": inventory[device]['lab-pass'],
+    "secret": inventory[device]['secret'],}
+    
+    try:
+        net_connect = ConnectHandler(**device_info)
+    except:
+        net_connect = False
+    #try to connect to the device via ssh, if it fails mark net_connect as false 
+    #this is used later to determine if the ssh connecton is alive or not
+    
+    try:
+        net_connect.enable()
+    except:
+        pass
+    #try to enter privilidged mode if the device type supports it
+    inventory[device]['ssh_con'] = net_connect
+    return inventory
+
 
 def close_ssh_con(inventory):
 #for all devices with an active ssh connection...
@@ -58,8 +80,8 @@ def run_configure_replace_command(inventory, device, config_replace_command):
 
 def single_dev_run_priv_cmd_raw_output(inventory, device, command):
 #run a show command against a device and return the outout as raw text
-    output = inventory['device']['ssh_con'].send_command(command)
-    inventory['device'][command] = output
+    output = inventory[device]['ssh_con'].send_command(command)
+    inventory[device][command] = output
     return inventory
 
 def dev_type_run_priv_cmds_raw_output(inventory, device_type, command):
@@ -121,7 +143,7 @@ def backup_all_devices():
                 #create file name based on device name and timestamp
                 output_file_data = lab_inventory[dev]['show run']
                 #create a string to hold the output for formatting
-                output_file_data= output_file_data.strip()
+                output_file_data = output_file_data.strip()
                 #strip off whitespace at the beginning and end of the file
                 lab_file_ops.write_string(output_file_path, output_file_name, output_file_data)
                 #write the output to a file using file_ops
@@ -146,3 +168,22 @@ def reset_lab_to_gold():
 
     close_ssh_con(lab_inventory)
     #close all open ssh sessions
+
+
+def save_new_gold_config(device):
+    lab_inventory = lab_file_ops.read_json_files('./device_info/inventory/', 'lab_device_inventory.json')
+    #open the lab inventory
+    lab_inventory = open_single_ssh_con(lab_inventory, device)
+    #attempt to establish ssh connections to all lab devices
+    lab_inventory = single_dev_run_priv_cmd_raw_output(lab_inventory, device, "wr mem")
+    command = f"more flash:{device}.gold"
+    lab_inventory = single_dev_run_priv_cmd_raw_output(lab_inventory, device, command)
+    #run more command to capture output of gold config on device
+    output_file_path = f'./device_info/device_configs/{device}/'
+    #path to save file
+    output_file_name = f'{device}-gold.txt'
+    #create file name based on dev name
+    output_file_data = lab_inventory[device][command]
+    output_file_data = output_file_data.strip()
+    #strip off whitespace at the beginning and end of the file
+    lab_file_ops.write_string(output_file_path, output_file_name, output_file_data)
--- virtual-box-lab/save_new_gold.py
+++ virtual-box-lab/save_new_gold.py
@@ -2,5 +2,6 @@
 import lab_device_ops
 
 if __name__ == "__main__":
-    lab_device_ops.reset_lab_to_gold()
+    device = input("Enter device name (case sensitive):")
+    lab_device_ops.save_new_gold_config(device)
     #currently only supports cisco devices
